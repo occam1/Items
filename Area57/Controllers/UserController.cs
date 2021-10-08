91460607861dc99c82136435256bdbd561918be7
Mark <mark_a_hall@yahoo.com>
2021-10-07 21:07:04 -0500

    Authorization part 2

---
+++ Area57/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Application.Controllers
+{
+    
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public IJwtAuthenticationManager _jwtAuthenticationManager { get; }
+
+        public UserController(IJwtAuthenticationManager jwtAuthenticationManager)
+        {
+            _jwtAuthenticationManager = jwtAuthenticationManager;
+        }
+        // GET: api/<UserController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<UserController>
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] UserCred userCred) 
+        {
+            var token = _jwtAuthenticationManager.Authenticate(userCred.userId, userCred.password);
+            if (token == null)
+            {
+                return Unauthorized();
+            }
+            return Ok(token);
+        }
+
+    }
+}
---
+++ Area57/Controllers/UserCred.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Controllers
+{
+    public class UserCred
+    {
+        public string userId { get; set; }
+        public string password { get; set; }
+    }
+}
